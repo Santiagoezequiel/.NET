803644a7fc3df309ae9009a28dd82ff8a2b543c6
Santiagoezequiel <s4ntiago.ezequiel37@gmail.com>
2024-05-10 18:41:15 -0300

    Clase2

---
+++ Clase2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Clase2
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+
+            checked
+            {                                       // Esto chequea que no pase por alto los errores de desbordamiento.
+
+ //           int numero = int.MaxValue;
+
+   //         int resultado=numero+numero;
+
+     //       Console.WriteLine(resultado);
+            }
+
+
+            try 
+            {
+                System.IO.StreamReader archivo = null;
+                string linea;
+                int contador = 0;
+                string path = @"C:\Users\Santi\Desktop\hola.txt";
+                archivo = new System.IO.StreamReader(path);
+
+                while ((linea=archivo.ReadLine())!= null)
+                {
+                    Console.WriteLine(linea);
+                    contador++;
+                
+
+
+                }
+
+
+            } catch (Exception ex)
+            {
+
+
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumaNumer(10, 40));
+
+        }
+
+        static double sumaNumer(int a, int b) => a + b;
+        static string sumaNumer(string a, int b) => a;
+
+        private static int imprimir(int abc, int cde)
+        {
+            return abc + cde;
+        }
+
+
+    }
+}
